8860ef1deda7d7e8a997a4612c91c8649a0a8b7b
jeffery graham <jeffery.lemar.graham@gmail.com>
2021-01-08 21:47:47 -0500

    useEffect

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -3,7 +3,29 @@ import React, { useState, useEffect } from 'react';
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    console.log('Call use effect');
+
+    if (value >= 1) {
+      document.title = `New Message(${value})`;
+    }
+  }, [value]);
+
+  useEffect(() => {
+    console.log('Hello World!');
+  }, []);
+
+  console.log('Render component');
+  return (
+    <>
+      <h2>{value}</h2>
+      <button className='btn' onClick={() => setValue(value + 1)}>
+        Increase
+      </button>
+    </>
+  );
 };
 
 export default UseEffectBasics;
