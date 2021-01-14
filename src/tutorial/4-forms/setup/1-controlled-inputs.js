2f1382a346adcbc9d91a9a06cd8e261f5ca9beb2
jeffery graham <jeffery.lemar.graham@gmail.com>
2021-01-13 23:03:49 -0500

    controlled inputs

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -6,7 +6,52 @@ import React, { useState } from 'react';
 // value, onChange
 
 const ControlledInputs = () => {
-  return <h1>controlled inputs</h1>;
+  const [firstName, setFirstName] = useState('');
+  const [email, setEmail] = useState('');
+  const [people, setPeople] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (firstName && email) {
+      const person = {
+        firstName,
+        email,
+      };
+      console.log(person);
+    } else {
+      console.log('Empty values');
+    }
+  };
+
+  return (
+    <>
+      <article>
+        <form className='form' onSubmit={handleSubmit}>
+          <div className='form-control'>
+            <label htmlFor='firstName'>Name : </label>
+            <input
+              type='text'
+              id='firstName'
+              name='firstName'
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+          </div>
+          <div className='form-control'>
+            <label htmlFor='email'>Email : </label>
+            <input
+              type='text'
+              id='email'
+              name='email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <button type='submit'>Add Person</button>
+        </form>
+      </article>
+    </>
+  );
 };
 
 export default ControlledInputs;
